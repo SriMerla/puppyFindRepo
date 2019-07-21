2c02b99c96452f91cc2b693e04674a4b9f3704a8
SriMerla <srimerla414@gmail.com>
2019-07-21 01:10:37 -0400

    JIRA_NUMBER - Updated Test Runner to generate cucumber.json for cucumber reports

--- src/test/java/Runner/TestRun.java
+++ src/test/java/Runner/TestRun.java
@@ -7,9 +7,13 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:/Users/srime/eclipse-workspace1/PuppyFind/src/test/java/Features/login.feature", 
-                 glue= {"StepDefinitions"}   
-                
-                 )
+plugin = { "pretty", 
+		"html:target/cucumber-html-report",
+		"json:target/cucumber.json",
+		"junit:target/cucumber.xml",
+		"rerun:target/rerun.txt"},
+        glue= {"StepDefinitions"}   
+       )
 public class TestRun {
 	
 
